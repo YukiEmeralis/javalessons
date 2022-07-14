eab5f1be81e01f45a5e767446ac88c7188e8ce7a
GARRETT <yukiemeralis@gmail.com>
2022-07-14 12:29:46 -0600

    071422.0

--- Lesson3.java
+++ Lesson3.java
@@ -1,54 +1,28 @@
-public class Lesson3 // <- Class scope identifier
-{ // Class scope start "{"
-	
-	// "Things" are written in the class scope, like variables...	
+public class Lesson3 
+{ 
 	static int aClassVariable = 0;
 	static Object[] someList = new Object[] {"Hello, world!", 42, null, false, 'a'};
 
-	// methods...
 	public static void main(String[] args)
-	{ // "main" method scope start
-	
-		// In method scopes, all scopes are treated as "instruction" scopes for
-		// conditions and loops and such
+	{ 
+		int aMethodVariable = 10;
 
-		if (aClassVariable == 0)
-		{ // Condition scope start
-
+		if (aClassVariable == 0 && aMethodVariable == 10)
+		{ 
 			for (Object o : someList)
-			{ // Loop scope start
+			{ 
 				exampleMethod(o);
-			} // Loop scope end
-
-		} // Condition scope end
-
-	} // "main" Method scope end
-
-
-	// more methods...
+			} 
+		} 
+	} 
+	
 	private void exampleMethod(Object o)
-	{ // "exampleMethod" method scope start
+	{ 
 
-	} // "exampleMethod" method scope end
-
-
-	// and even classes nested inside the given class
+	} 
+	
 	private static class NestedClass
-	{ // "NestedClass" class scope start
+	{ 
 
-	} // "NestedClass" class scope end
-} // Class scope end "}"
-
-// All in all, this class's structure is 
-// class start
-// - "main" method start
-// - - if statement start
-// - - - for loop start
-// - - - for loop end
-// - - if statement end
-// - "main" method end
-// - "exampleMethod" method start
-// - "exampleMethod" method end
-// - "NestedClass" class start
-// - "NestedClass" class end
-// class end
+	} 
+} 
